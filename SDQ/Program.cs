152ef27812f0525339eb8adb7573fa76270784b0
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-05-30 14:25:38 -0300

    Creating Scorer solution

---
+++ SDQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Read all files in directory
+            // TODO For each file, generate analysis file
+        }
+    }
+}
