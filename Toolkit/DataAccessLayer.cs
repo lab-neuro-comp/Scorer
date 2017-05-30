c30d0117990ce3f8823620c7c9ceee2a1b5cbb39
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-05-30 16:14:23 -0300

    Adding docs to toolkit functions

--- Toolkit/DataAccessLayer.cs
+++ Toolkit/DataAccessLayer.cs
@@ -9,11 +9,22 @@ namespace Toolkit
 {
     public class DataAccessLayer
     {
+        /// <summary>
+        /// Get all files in the given directory. Does not check inside subdirectories.
+        /// </summary>
+        /// <param name="src">The directory to get the files</param>
+        /// <returns>An array of file and dir names</returns>
         public static string[] AllFiles(string src)
         {
             return Directory.GetFiles(src);
         }
 
+        /// <summary>
+        /// Check if a string is the end of the other. Order does not matter here.
+        /// </summary>
+        /// <param name="s">First string</param>
+        /// <param name="t">Second string</param>
+        /// <returns>The promised comparison.</returns>
         public static bool HaveSameEndings(string s, string t)
         {
             int sl = s.Length;
@@ -32,6 +43,11 @@ namespace Toolkit
             return flag;
         }
 
+        /// <summary>
+        /// Loads the given file in memory.
+        /// </summary>
+        /// <param name="filename">The file name.</param>
+        /// <returns>The file contents, or null if the file does not exist.</returns>
         public static string LoadFile(string filename)
         {
             return (File.Exists(filename))? File.ReadAllText(filename) : null;
