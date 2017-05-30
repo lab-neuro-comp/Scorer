a71308ab17a55919966953c094b693ff4dca204a
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-05-30 15:13:49 -0300

    Adding unit tests

---
+++ Testing/TestingSDQ.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{ 
+    [TestFixture]
+    public class TestingSDQ
+    {
+        [Test]
+        public void TestFullProgram()
+        {
+            string[] args = { @"..\..\..\Data" };
+            Assert.DoesNotThrow(() =>
+            {
+                SDQ.Program.Main(args); 
+            });
+        }
+    }
+}
