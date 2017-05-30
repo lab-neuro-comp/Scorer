586350dbc191604266f49f84d6e5644680172bed
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2017-05-30 14:35:36 -0300

    Getting all files in a directory

---
+++ Toolkit/DataAccessLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit
+{
+    public class DataAccessLayer
+    {
+        public static string[] AllFiles(string src)
+        {
+            return Directory.GetFiles(src);
+        }
+    }
+}
